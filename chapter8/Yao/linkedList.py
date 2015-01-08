e25e38ad458de16ae10953f24122c5eabe77dc12
Yale Zhao <Yale@Yale.local>
2015-01-08 15:10:00 -0700

    Define functions for creating list

---
+++ chapter8/Yao/linkedList.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# define the Node class and provide functions to create and print a linked list
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+def createList(a, b):
+    aList = Node(a)
+    iter = aList
+    for i in range(a + 1, b):
+        iter.next = Node(i)
+        iter = iter.next
+    return aList
+
+def printList(aList):
+    iter = aList
+    while iter:
+        print(iter.value, end = " ")
+
